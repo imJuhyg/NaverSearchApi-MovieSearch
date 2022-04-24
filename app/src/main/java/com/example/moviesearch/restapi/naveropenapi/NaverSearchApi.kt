9b65bde4311eaa85c7832390e1b9b7bc7edd008c
Juhyeong-Lim <limjuhyg@gmail.com>
2022-04-24 20:03:33 +0900

    Refactor code structure

--- app/src/main/java/com/example/moviesearch/restapi/naveropenapi/NaverSearchApi.kt
+++ app/src/main/java/com/example/moviesearch/restapi/naveropenapi/NaverSearchApi.kt
@@ -1,7 +1,10 @@
 package com.example.moviesearch.restapi.naveropenapi
 
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface NaverSearchApi {
     /**
